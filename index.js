de314621af6b707a6f64e93c4c1d236162dccd04
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-05-26 12:55:03 +0200

    Code skeleton

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
+// noinspection ExceptionCaughtLocallyJS
+
 const core = require('@actions/core');
+const github = require('@actions/github');
+
+const YAML = require('yaml');
+
+const client = require('@nemerosa/ontrack-github-action-client');
 
 async function run() {
     try {
         const logging = core.getBooleanInput('logging');
+
+        let clientEnvironment = client.checkEnvironment(logging);
+        if (!clientEnvironment) {
+            core.info("Ontrack is not configured. Not doing anything.");
+            return;
+        } else if (logging) {
+            core.info(`Ontrack URL: ${clientEnvironment.url}`)
+        }
+
+        // Getting all the arguments
+        let owner = core.getInput('owner');
+        let repository = core.getInput('repository');
+        const buildName = core.getInput('build-name');
+        const buildLabel = core.getInput('build-label');
+
+        // Setting the owner
+        if (!owner) {
+            owner = github.context.repo.owner;
+        }
+
+        // Setting the repository
+        if (!repository) {
+            repository = github.context.repo.repo;
+        }
+
+        // Build name & label cannot be specified together
+        if (buildName && buildLabel) {
+            throw Error('"build-name" and "build-label" cannot be both declared.');
+        }
+
+        // Using build links
+        const buildLinksYaml = core.getInput('build-links');
+        if (buildLinksYaml) {
+            const buildLinks = YAML.parse(buildLinksYaml);
+            // TODO Calls the GraphQL client
+            await client.graphQL(clientEnvironment, '', {});
+        }
+
     } catch (error) {
         core.setFailed(error.message);
     }
